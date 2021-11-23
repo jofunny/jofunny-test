8bd808c4296ea1f4af672a2017935d7c6d62cc54
jofunny <94912151+jofunny@users.noreply.github.com>
2021-11-23 23:04:45 +0800

    Update git_test.java

--- src/main/java/com/atguigu/git/git_test.java
+++ src/main/java/com/atguigu/git/git_test.java
@@ -13,6 +13,7 @@ public class git_test {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
-        System.out.println("oush test");
+        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
