0d08451d5e0d08555ab11593256c6ce1ea63daaf
Layne <Layne@atguigu.com>
2021-11-23 20:49:06 +0800

    second commit

--- src/main/java/com/atguigu/git/git_test.java
+++ src/main/java/com/atguigu/git/git_test.java
@@ -8,5 +8,6 @@ package com.atguigu.git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
