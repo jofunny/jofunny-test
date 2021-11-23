48adc58a4e486167da0c9e992442b4ee40f50a50
Layne <Layne@atguigu.com>
2021-11-23 20:45:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/git_test.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author laowang@365
+ * @create 2021-11-2320:36
+ * @documentation
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
